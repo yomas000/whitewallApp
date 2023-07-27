6c0266e9766d774c852ed513a593c75af4892009
Thomas <thomas.ed.dick@gmail.com>
2023-07-27 17:26:24 -0600

    update compile script

--- compile.sh
+++ compile.sh
@@ -2,6 +2,8 @@
 
 npm install
 
+export ANDROID_HOME=/opt/android-sdk
+
 echo ""
 echo "Setting Icon"
 npx react-native set-icon --path  ../$1
@@ -12,5 +14,11 @@ cd android
 chmod +x gradlew
 
 ./gradlew bundleRelease
+./gradlew assembleRelease
 
 cp app/build/outputs/bundle/release/app-release.aab ../../app-release.aab
+cp app/build/outputs/apk/release/app-release.apk ../../app-release.apk
+
+echo "enter sudo password to delete app directory"
+cd ../../
+rm -R ../whitewallApp
